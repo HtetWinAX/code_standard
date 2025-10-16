0197e29d95aa9f6a31a759ddf12fb27eac136a70
HtetWin <htetaungwin.asiamatrix@gmail.com>
2025-10-16 15:15:15 +0630

    [add]

---
+++ account_extend/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Account Extend",
+    "version": "17.0.1.2.0",
+    "summary": "Accounting extend",
+    "category": "account",
+    "author": "Asia Matrix Software Solution",
+    "website": "https://www.asiamatrixsoftware.com",
+    "email": "info@asiamatrixsoftware.com",
+    "license": "LGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_views.xml",
+    ],
+    # "installable": True,
+    # "application": False,
+    # "auto_install": False,
+}
